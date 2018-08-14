f69389eba22147d0c01642d7633b1d3d6e707ef3
Singh <yash5@ls-cns-web05.clas.ufl.edu>
2018-08-14 10:14:28 -0400

    30th commit

--- home.php
+++ home.php
@@ -89,21 +89,23 @@ get_header(); ?>
 <?php echo do_shortcode('[STARTCOL class="p33"]'); ?>
 
 <div class="X-tile">
+   <div class="X-tile">
    <div class="X-tile-img"> 
-      <img src="https://t.publications.clas.ufl.edu/wp-content/uploads/2018/02/taylor-768x768.jpg" alt="" width="736" height="535" class="alignnone size-thumbnail-crop wp-image-64" />
-      <div class="X-tile-txt"><a href="" class="">Taylor Rouviere ’18</a></div>
+    
+      <img src="https://test.clas.ufl.edu/themedev/files/2018/08/Nick_Nyiragongo-768x768.jpeg" alt="" class="alignnone size-thumbnail-crop wp-image-64" />
+      <div class="X-tile-txt"><a href="https://publications.clas.ufl.edu/college-newsletter/las-winter-2018/renaissance-man/" class="">Nick Dowhaniuk PhD’21</a></div>
    </div>  
    <div class="X-tile-desc"> 
        <p>Pursuing both a PhD in geography and a Master of Health Science degree at UF means that Dowhaniuk indeed does quite a bit of office work, but he is no stranger to adventure. A National Geographic Explorer, Dowhaniuk studies the sociocultural and spatial effects of oil development in Uganda, as well as conservation issues there and in South Africa. His dissertation research on Ugandans’ access to healthcare serves his career goal of founding an NGO devoted to community-based health intervention.</p>
  <a class="read-more" style="text-transform:uppercase;" href="https://publications.clas.ufl.edu/college-newsletter/las-winter-2018/renaissance-man/">Read More About Nick</a>
-</div>
+    </div>
 </div>
 
 <?php echo do_shortcode('[ENDCOL]'); ?>
 <?php echo do_shortcode('[STARTCOL class="p33"]'); ?>
 <div class="X-tile">
    <div class="X-tile-img"> 
-      <img src="https://t.publications.clas.ufl.edu/wp-content/uploads/2018/01/20160319_NYT_KeyLargo_Day2_1601-768x768.jpg" alt="" width="736" height="535" class="alignnone size-thumbnail-crop wp-image-63" />
+      <img src="https://t.publications.clas.ufl.edu/wp-content/uploads/2018/01/20160319_NYT_KeyLargo_Day2_1601-768x768.jpg" alt="" class="alignnone size-thumbnail-crop wp-image-63" />
       <div class="X-tile-txt"><a href="">Andrea Dutton</a></div>
    </div>  
     <div class="X-tile-desc"> 
@@ -114,15 +116,18 @@ get_header(); ?>
 <?php echo do_shortcode('[ENDCOL]'); ?>
 <?php echo do_shortcode('[STARTCOL class="p33"]'); ?>
 <div class="X-tile">
-   <div class="X-tile-img"> 
-      <img src="https://t.publications.clas.ufl.edu/wp-content/uploads/2018/02/Hernandez_BuckyBall-768x768.jpg" alt="" width="736" height="535" class="alignnone size-thumbnail-crop wp-image-62" />
-      <div class="X-tile-txt"><a href="">Joe Hernandez ’96</a></div>
+     <div class="X-tile-img"> 
+      <img src="https://test.clas.ufl.edu/themedev/files/2018/08/2017-04-04_Mindfulness_talk-3625-1440x2160-768x768.jpg" alt=""  class="alignnone size-thumbnail-crop wp-image-62" />
+
+      <div class="X-tile-txt"><a href="https://publications.clas.ufl.edu/college-newsletter/las-summer-2017/in-the-moment/">Trish Ring MS'93, PhD'96</a></div>
    </div>  
-   <div class="X-tile-desc"><p>
-Eget nunc lobortis mattis aliquam faucibus purus in. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Purus in massa tempor nec feugiat nisl pretium fusce. Imperdiet proin fermentum leo vel orci porta non pulvinar. Felis donec et odio pellentesque diam volutpat. Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. Nulla aliquet enim tortor at auctor urna nunc. Nisl nisi scelerisque eu ultrices vitae auctor. Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus. Rhoncus dolor purus non enim. Enim ut sem viverra aliquet.
-</p>
-<a class="read-more" href="https://t2.publications.clas.ufl.edu/college-news/space-test/">READ MORE</a>
-   </div>
+  <div class="X-tile-desc"><p>
+ Trish Ring draws a distinction between “sorrow and suffering,” believing the first is part of life and the second is unnecessary. She’s a Certified Equus Coach who has her clients talk through their concerns in a round pen with horses, which are very sensitive to human emotions. “When there’s a 2,000-pound animal in the ring with you, it shakes up your framework a bit,” she explains. Her program takes place at Blue Star Ranch, which she and her husband Carl created out of “several derelict plots of land” to create a refuge for people to enjoy nature and seek self-improvement. The ranch is the largest equine coaching facility in the Southeast.
+ 
+ </p>
+ <a class="read-more" style="text-transform:uppercase;" href="https://publications.clas.ufl.edu/college-newsletter/las-summer-2017/in-the-moment/">Read More About Trish</a>
+    </div>
+</div>
 </div>
 <?php echo do_shortcode('[ENDCOL]'); ?>
 <?php echo do_shortcode('[ENDROW]'); ?>
