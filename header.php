971b3b72ac81b546efe970363dac50b63de06cc8
Singh <yash5@ls-cns-web05.clas.ufl.edu>
2018-09-05 14:30:51 -0400

    78 commit

--- header.php
+++ header.php
@@ -12,6 +12,14 @@
  * @version 1.0
  */
 
+
+/* TEMPORARY: REMOVE ONCE LIVE   */
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0"); // Proxies.
+
+
+
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
