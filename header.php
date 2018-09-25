739bce53d3143c58f9ef9a632b1779c4f3944488
Singh <yash5@ls-cns-web05.clas.ufl.edu>
2018-09-25 16:06:34 -0400

    H1 entry title edits

--- header.php
+++ header.php
@@ -8568,7 +8568,7 @@ h2.right-arrow  strong::before {
                          $post_id = $post->ID; // current post ID
                          $var_sub_head=get_field('sub_head');
                          if ( !is_null($var_sub_head) )
-                            echo '<h2 class="dddd">'.$var_sub_head.'</h2>';
+                            echo '<h2>'.$var_sub_head.'</h2>';
                        } 
 
                       if( get_field('byline') ){   
@@ -8603,7 +8603,7 @@ h2.right-arrow  strong::before {
                          // this will throw a MAJOR error if the advanced custom fields plugin is not installed, fix later.
                          $var_sub_head=get_field('sub_head');
                          if ( !is_null($var_sub_head) )
-                            echo '<h2>'.$var_sub_head.'</h2>';
+                            echo '<h2  class="dddd">'.$var_sub_head.'</h2>';
 
                    }
 	   echo '</div></div>';
