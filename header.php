b142fa910a898487b408b8bed8fd92b5c6407a2f
Singh <yash5@ls-cns-web05.clas.ufl.edu>
2018-09-28 16:40:14 -0400

    DROP DOWN ARROW EDITS

--- header.php
+++ header.php
@@ -8136,7 +8136,7 @@ h2.right-arrow  strong::before {
  {
     background-image: url('images/arrow-down.png');
     background-repeat: no-repeat;
-    padding: 10px 0 10px 16px;
+    padding: 10px 10px 10px 16px;
     cursor: pointer;
 
 
