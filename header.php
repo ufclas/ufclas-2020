1e3d62379a50d7ad6b82cb079a882ca5179b75bb
Singh <yash5@ls-cns-web05.clas.ufl.edu>
2018-10-04 10:57:49 -0400

    Footer edits

--- header.php
+++ header.php
@@ -5718,7 +5718,7 @@ a.page-numbers  .icon-arrow-left:hover {
             position:absolute;
             display:table;   
             width:100%;
-            height: 6.5vw;
+            height: 8vw;
             background:rgba(0,52,108,.8);
             bottom:0px;  
             text-align:center; 
