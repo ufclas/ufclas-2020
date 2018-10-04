a3ba1e44cf3d9ad0d55f26732491b8f887ed3f17
Singh <yash5@ls-cns-web05.clas.ufl.edu>
2018-10-04 10:56:41 -0400

    Footer edits

--- header.php
+++ header.php
@@ -5727,7 +5727,7 @@ a.page-numbers  .icon-arrow-left:hover {
             display:block;
             width:100%;
             height:100%;
-            font-size:1.2rem;       
+            font-size:1.4rem;        
             font-family:Quadon_Medium;
             color:#ffffff !important;
             text-align:left;
