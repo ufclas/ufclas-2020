c834a688c9a597140787568a95e79e7cb33c7862
Singh <yash5@ls-cns-web05.clas.ufl.edu>
2018-10-04 10:52:51 -0400

    Footer edits

--- header.php
+++ header.php
@@ -5590,6 +5590,7 @@ a.page-numbers  .icon-arrow-left:hover {
             display:table;   
             width:100%;
             height: 6.5vw;
+            max-height: 130px;
             background:rgba(0,52,108,.8);
             bottom:0px;  
             text-align:center; 
